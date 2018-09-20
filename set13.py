c6c34595be8a71f709bb7f6bb4644dd84492aa77
priyakrisv <43440633+priyakrisv@users.noreply.github.com>
2018-09-20 17:54:33 +0530

    Create set13.py

---
+++ set13.py
@@ -0,0 +1,7 @@
+l=input("input a letter of a alphapet:")
+if(l in('a','e','i','o','u')):
+    print("%s is a vowel."% l) 
+elif(l=='y'):
+    print("sometimes y stand for vowel,stand for consonant.")    
+else:
+    print("%s is a consonant."% l)
