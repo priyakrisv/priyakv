067a40608b4adad746545d6133b36f6dfe9219ab
priyakrisv <43440633+priyakrisv@users.noreply.github.com>
2018-09-24 17:11:23 +0530

    Create set15.py

---
+++ set15.py
@@ -0,0 +1,10 @@
+n1=input("enter any number")
+n2=input("enter any number")
+n3=input("enter any number")
+if((n1>=n2) and (n1>=n3)):
+    largest=n1
+elif((n2>=n1) and (n2>=n3)):
+    largest=n2 
+else:
+    largest=n3
+    print("the largest number between",n1,",",n2,",","and",n3,"is",largest)    
