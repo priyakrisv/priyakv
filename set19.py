87107d4d33792d446e5932d8087b075e35ce992b
priyakrisv <43440633+priyakrisv@users.noreply.github.com>
2018-09-25 10:39:41 +0530

    Create set19.py

---
+++ set19.py
@@ -0,0 +1,19 @@
+consider k=4,n=9
+given array:312221433
+i=0
+        3__
+temp[] has one element,3 with count 1
+i=1      
+        31_
+temp[] has two element,3 and with  1
+counts 1 and 1 respectively
+i=2      
+        312
+temp[] has three element,3 and with  1
+count as1,1 and 1 respectively
+i=3  
+        __2
+        312
+temp[] has three element,3 and with  1
+count as1,1 and 1 respectively
+
