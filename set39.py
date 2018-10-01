3b7125bdf82a1d31e532ef146c68e8f1c43c7121
priyakrisv <43440633+priyakrisv@users.noreply.github.com>
2018-10-01 15:15:08 +0530

    Update set39.py

--- set39.py
+++ set39.py
@@ -1,3 +1,13 @@
 SECONDS PER MINUTE=60
 SECONDS PER HOUR=3600
+SECONDS PER DAY=86400
+seconds=int(input("enter number of seconds:"))
+days=seconds / SECONDS PER DAY
+seconds=seconds % SECONDS PER DAY
+hours=seconds / SECONDS PER HOUR
+seconds=seconds % SECONDS PER HOUR
+minutes=seconds / SECONDS PER MINUTE 
+seconds=seconds % SECONDS PER MINUTE
+print("duration is:","%d:%02d:%02d:%02d%(days,hours,minutes,seconds)")
+
  
