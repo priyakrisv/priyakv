2cce526fe757159c874ae0f9af85bd51f77a36d2
priyakrisv <43440633+priyakrisv@users.noreply.github.com>
2018-10-10 16:41:03 +0530

    Create set41.py

---
+++ set41.py
@@ -0,0 +1,11 @@
+str=input("enter string:")
+char=0
+word=0
+for i in str:
+char=char+1
+if(i==''):
+word=word+1
+print("number of words in the string:")
+print(word)
+print("number of  characters in the string:")
+print(char)
