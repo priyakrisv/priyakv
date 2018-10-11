4bf9fe66fb043c37fc4ae50c8c5202028da9b876
priyakrisv <43440633+priyakrisv@users.noreply.github.com>
2018-10-11 17:36:46 +0530

    Create set52.py

---
+++ set52.py
@@ -0,0 +1,8 @@
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+if (str1 == str2):
+    print("\nBoth strings are equal to each other.")
+    print(str1,"==",str2);
+else:
+    print("\nStrings are not equal.")
+    print(str1,"!=",str2);
