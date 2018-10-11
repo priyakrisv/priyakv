5e2cdd5b55a16cffd79bf48ea4cc000a113b0aa2
priyakrisv <43440633+priyakrisv@users.noreply.github.com>
2018-10-11 17:00:31 +0530

    Create set48.py

---
+++ set48.py
@@ -0,0 +1,5 @@
+a =int(input("enter any number:"))
+b =int(input("enter any number:"))
+print("\nBefore swap a = %d and b = %d" %(a, b))
+a, b = b, a
+print("\nAfter swaping a = %d and b = %d" %(a, b))
