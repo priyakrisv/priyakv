d8c86d42bda264887ee066d3859610f93c3b2bb4
priyakrisv <43440633+priyakrisv@users.noreply.github.com>
2019-01-09 14:22:34 +0530

    Update set61.py

--- set61.py
+++ set61.py
@@ -1,6 +1,6 @@
-p =int(input("Enter number:"))
+p=int(input("Enter number:"))
 count=0
 while(p>0):
-    count=count+1
-    p=p//10
- print("The number of digits in the number are:",count)
+count=count+1
+ p=p//10
+ print(" number of digits in the number are:",count)
